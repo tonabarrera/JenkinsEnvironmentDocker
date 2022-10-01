8ce5ab5e7326b8b17aadb28cf94145a5d8709706
Tona <carlostonatihu@gmail.com>
2022-10-01 15:05:45 -0500

    De nuevo pruebas

--- GatlingTest/src/test/scala/microservice/PingUsersSimulation.scala
+++ GatlingTest/src/test/scala/microservice/PingUsersSimulation.scala
@@ -42,23 +42,16 @@ class PingUsersSimulation extends Simulation {
           session}}
 
   object BusinessLogic {
-    var headers_10 = Map("Content-Type" -> "application/json; charset=ISO-8859-1",
-                          /*"Authorization" -> "Bearer ${access}"*/)
-                          /*
-    exec { session =>
-      println(session("access").as[String])
-      session
-    }*/
+    var headers_10 = Map("Content-Type" -> "application/json; charset=ISO-8859-1")
+    
     val getPing =
-      /*exec(session => session.set("access", token))
-      .*/exec(http("GetPing")
+      exec(http("GetPing")
         .get("/v1/service/microservicio/ping")
         .headers(headers_10)
         .check(status.is(200))
       )
     val getUsers =
-      /*exec(session => session.set("access", token))
-      .*/exec(http("GetUsers")
+      exec(http("GetUsers")
         .get("/stress/users")
         .headers(headers_10)
         .check(status.is(200))
@@ -72,7 +65,6 @@ class PingUsersSimulation extends Simulation {
     )
 
   setUp(
-    /*auth.inject(constantUsersPerSec(1) during (1 seconds)),*/
     scn.inject(
       nothingFor(2 seconds),
       atOnceUsers(5),
